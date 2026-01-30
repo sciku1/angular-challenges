0e1284e3cad5c31b87b067deedd203c0f44b6aa2
Tim Lopez <tim.lopez@arcurve.com>
2026-01-29 22:04:16 -0700

    Challenge:52

--- apps/angular/52-lazy-load-component/src/app/app.component.ts
+++ apps/angular/52-lazy-load-component/src/app/app.component.ts
@@ -4,9 +4,9 @@ import { Component, signal } from '@angular/core';
   selector: 'app-root',
   template: `
     <div class="h-screen bg-gray-500">
-      @if (topLoaded()) {
+      @defer {
         <app-top />
-      } @else {
+      } @placeholder {
         <app-placeholder />
         <button
           class="rounded-sm border border-blue-500 bg-blue-300 p-2"
